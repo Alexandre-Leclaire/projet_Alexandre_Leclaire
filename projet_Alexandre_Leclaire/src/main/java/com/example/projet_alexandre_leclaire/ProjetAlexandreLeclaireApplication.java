a5d87ecc40189562716f9b30546d22af5478e04a
alexandre.leclaire <alexandre.leclaire@epita.fr>
2025-11-20 14:19:07 +0100

    init projet

---
+++ projet_Alexandre_Leclaire/src/main/java/com/example/projet_alexandre_leclaire/ProjetAlexandreLeclaireApplication.java
@@ -0,0 +1,13 @@
+package com.example.projet_alexandre_leclaire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetAlexandreLeclaireApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjetAlexandreLeclaireApplication.class, args);
+    }
+
+}
